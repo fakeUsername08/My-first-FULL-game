9b9171688b0c42ab14ac1b20d0713352385c2b79
fakeUsername08 <fake.gamer8998@gmail.com>
2023-03-29 19:23:53 +0430

    mass

--- doge_enemy.py
+++ doge_enemy.py
@@ -12,9 +12,13 @@ fps = 60
 player_x = 240
 flag = True
 speed = 10
+enemy_speed = speed/3
+level_limit = 10
+level_show = 1
 sccore = 0
 font = pygame.font.Font("DS-DIGI.TTF",30)
 font_end = pygame.font.Font("DS-DIGI.TTF",60)
+flag_music = False
 
 # game
 win = pygame.display.set_mode(size)
@@ -37,11 +41,10 @@ enemy = pygame.image.load("pictrue/rock.png")
 enemy_new = pygame.transform.scale(enemy,(80,80))
 
 
-
 # function
 def make_enemy():
     spawn = random.randint(1,50)
-    if len(enemy_list) < 10 and spawn == 1:
+    if len(enemy_list) < 9 and spawn == 1:
         enemy_x = random.randint(0,520)
         enemy_pos = [enemy_x,30]
         enemy_list.append(enemy_pos)
@@ -53,9 +56,12 @@ def spawn_enemy():
 
 def update_enemy():
     for index,enemy_xy in enumerate(enemy_list):
-        enemy_xy[1] += speed/3
+        enemy_xy[1] += enemy_speed
         if enemy_xy[1] > 800:
             enemy_list.pop(index)
+            pygame.mixer.music.load("sound/coin.wav")
+            pygame.mixer.music.set_volume(0.5)
+            pygame.mixer.music.play()
             return True
 
 def game_over():
@@ -73,6 +79,10 @@ def waiting():
         pygame.display.update()
         time.sleep(1)
 
+def level():
+    if sccore == level_limit:
+        return True
+
 # loop
 while 1:
     for event in pygame.event.get():
@@ -86,16 +96,27 @@ while 1:
             if (event.key == pygame.K_LEFT or event.key == pygame.K_a) and player_x > 0:
                 player_x -= speed
                 # print(player_x)
-
+    if not flag_music:
+        flag_music = True
+        pygame.mixer.music.load("sound/scifi.wav")
+        pygame.mixer.music.set_volume(0.4)
+        pygame.mixer.music.play()
     text = (f"sccore: {sccore}")
     text_show = font.render(text,False,white)
+    text_2 = (f"level: {level_show}")
+    text_2_show = font.render(text_2,False,white)
     clock.tick(fps)
+    if level():
+        enemy_speed += 0.5
+        level_limit += 11
+        level_show += 1
     make_enemy()
     win.blit(back_new,(-300,0))
     if flag:
         flag = False
         waiting()
     win.blit(text_show,(0,0))
+    win.blit(text_2_show,(0,30))
     spawn_enemy()
     point = update_enemy()
     if point:
@@ -105,16 +126,21 @@ while 1:
         win.blit(back_new,(-300,0))
         spawn_enemy()
         win.blit(player_crack_new,(player_x,700))
+        pygame.mixer.music.load("sound/player_death.wav")
+        pygame.mixer.music.set_volume(0.4)
+        pygame.mixer.music.play()
         pygame.display.update()
         time.sleep(2)
         for i in range(5,0,-1):
             end_screen = font_end.render("Game Over",False,orange)
             end_sccore = font_end.render(f"sccore: {sccore}",False,orange)
+            end_level = font_end.render(f"level: {level_show}",False,orange)
             end_wait = font_end.render(str(i),False,orange)
             win.blit(back_new,(-300,0))
             win.blit(end_screen,(200,350))
             win.blit(end_sccore,(200,400))
-            win.blit(end_wait,(200,450))
+            win.blit(end_level,(200,450))
+            win.blit(end_wait,(200,500))
             pygame.display.update()
             time.sleep(1)
         break
